b115e057e30897beaaf63a572e0000827dc8cb93
Peter Jihoon Kim <pete.kim@coinbase.com>
2020-08-25 23:36:47 +0000

    Refactor Network and ERC20

---
+++ src/util/address.test.ts
@@ -0,0 +1,100 @@
+import * as address from "./address";
+
+test("isValidAddress", () => {
+  expect(
+    address.isValidAddress("0x0000000000000000000000000000000000000000")
+  ).toBe(true);
+  expect(
+    address.isValidAddress("0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+  ).toBe(true);
+  expect(
+    address.isValidAddress("0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  ).toBe(true);
+  expect(
+    address.isValidAddress("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa")
+  ).toBe(true);
+
+  expect(
+    address.isValidAddress("0xAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaa")
+  ).toBe(false);
+
+  expect(address.isValidAddress("")).toBe(false);
+  expect(address.isValidAddress("0x")).toBe(false);
+  expect(
+    address.isValidAddress("0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabb")
+  ).toBe(false);
+
+  expect(address.isValidAddress(undefined as any)).toBe(false);
+  expect(address.isValidAddress(null as any)).toBe(false);
+  expect(address.isValidAddress(123 as any)).toBe(false);
+});
+
+test("checksumAddress", () => {});
+
+test("ensureValidAddress", () => {
+  expect(
+    address.ensureValidAddress("0x0000000000000000000000000000000000000000")
+  ).toEqual("0x0000000000000000000000000000000000000000");
+  expect(
+    address.ensureValidAddress("0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+  ).toEqual("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa");
+  expect(
+    address.ensureValidAddress("0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  ).toBe("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa");
+  expect(
+    address.ensureValidAddress("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa")
+  ).toBe("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa");
+
+  expect(() =>
+    address.ensureValidAddress("0xAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaa")
+  ).toThrow(address.InvalidAddressError);
+
+  expect(() => address.ensureValidAddress("")).toThrow(
+    address.InvalidAddressError
+  );
+  expect(() => address.ensureValidAddress("0x")).toThrow(
+    address.InvalidAddressError
+  );
+  expect(() =>
+    address.ensureValidAddress("0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabb")
+  ).toThrow(address.InvalidAddressError);
+
+  expect(() => address.ensureValidAddress(undefined as any)).toThrow(
+    address.InvalidAddressError
+  );
+  expect(() => address.ensureValidAddress(null as any)).toThrow(
+    address.InvalidAddressError
+  );
+  expect(() => address.ensureValidAddress(123 as any)).toThrow(
+    address.InvalidAddressError
+  );
+});
+
+test("checksumAddress", () => {
+  expect(
+    address.checksumAddress("0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+  ).toEqual("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa");
+  expect(
+    address.checksumAddress("0xAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaa")
+  ).toEqual("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa");
+  expect(
+    address.checksumAddress("0x33907bb48ad33ac84c2dc904715ff36facfe81a3")
+  ).toEqual("0x33907bB48AD33aC84C2dC904715FF36faCfe81a3");
+
+  expect(() => address.checksumAddress("")).toThrow(
+    address.InvalidAddressError
+  );
+  expect(() => address.checksumAddress("0x")).toThrow(
+    address.InvalidAddressError
+  );
+
+  expect(() => address.checksumAddress(undefined as any)).toThrow(
+    address.InvalidAddressError
+  );
+  expect(() => address.checksumAddress(null as any)).toThrow(
+    address.InvalidAddressError
+  );
+  expect(() => address.checksumAddress(123 as any)).toThrow(
+    address.InvalidAddressError
+  );
+});
--- src/util/address.ts
+++ src/util/address.ts
@@ -1,19 +1,54 @@
 import { EthereumAddress } from "wallet.ts";
 
+export class InvalidAddressError extends Error {
+  public constructor(public readonly address: unknown) {
+    super("Invalid address");
+    Object.setPrototypeOf(this, InvalidAddressError.prototype);
+    this.name = "InvalidAddressError";
+  }
+}
+
 /**
  * Checks the validity of an Ethereum address
  * @param address Ethereum address
  * @returns True if valid
  */
 export function isValidAddress(address: string): boolean {
+  if (typeof address !== "string") {
+    return false;
+  }
   return EthereumAddress.isValid(address);
 }
 
 /**
  * Converts the Ethereum address to a mixed-case checksum address (EIP-55)
  * @param address Ethereum address
+ * @throws InvalidAddressError
  * @returns Checksum address
  */
 export function checksumAddress(address: string): string {
-  return EthereumAddress.checksumAddress(address);
+  if (typeof address !== "string") {
+    throw new InvalidAddressError(address);
+  }
+  let checksumAddress: string;
+  try {
+    checksumAddress = EthereumAddress.checksumAddress(address);
+  } catch {
+    throw new InvalidAddressError(address);
+  }
+  return checksumAddress;
+}
+
+/**
+ * Checks the validity of an Ethereum address, returns a checksum address if
+ * the address is valid, and throws otherwise
+ * @param address Ethereum address
+ * @throws InvalidAddressError
+ * @returns Checksum address
+ */
+export function ensureValidAddress(address: string): string {
+  if (!isValidAddress(address)) {
+    throw new InvalidAddressError(address);
+  }
+  return checksumAddress(address);
 }
